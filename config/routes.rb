bbd79936fe276efb6614fb49e711cb0498603e54
rishabh-vinsol <rishabh.kaushik@vinsol.com>
2023-03-22 18:24:59 +0530

    Added orders and checkout features

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
   root "store#index", as: "store_index"
-  resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :products do 
+    get :who_bought, on: :member
+  end
 end
