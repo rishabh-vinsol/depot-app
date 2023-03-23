48357ca860385ab4c592c1c54628e1d1344ef70a
rishabh-vinsol <rishabh.kaushik@vinsol.com>
2023-03-23 15:24:58 +0530

    Added sidebar for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
+class Error < StandardError
+end
+
 class User < ApplicationRecord
+  after_destroy :ensire_an_admin_remains
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  private
+
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise Error.new "Can't delete last user"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  get "admin" => "admin#index"
+  controller :sessions do
+    get "login" => :new
+    post "login" => :create
+    delete "logout" => :destroy
+  end
+  get "sessions/create"
+  get "sessions/destroy"
   resources :users
   resources :orders
   resources :line_items
   resources :carts
   root "store#index", as: "store_index"
-  resources :products do 
+  resources :products do
     get :who_bought, on: :member
   end
 end
