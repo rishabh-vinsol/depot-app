92a99077da14682922b553f8eaeb9b1bf606f44d
rishabh-vinsol <rishabh.kaushik@vinsol.com>
2023-03-30 13:53:21 +0530

    Added translation in products.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,14 +10,14 @@ class Product < ApplicationRecord
   before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates_length_of :words_in_description, minimum: 5, maximum: 10,
-                                             too_short: "Description is too short (minimum is %{count} characters)",
-                                             too_long: "Description is too long (maximum is %{count} characters)"
+                                             too_short: I18n.t("product.description.too_short_message"),
+                                             too_long: I18n.t("product.desctiption.too_long_message")
   validates :price, allow_blank: true, numericality: { greater_than: :discount_price }
   # validate :price_greater_than_discount_price
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, url: true
   validates :permalink, uniqueness: true, format: { with: PERMALINK_FORMAT }
-  validates_length_of :words_in_permalink, minimum: 3, message: "Permalink is too short (minimum is 3 characters) "
+  validates_length_of :words_in_permalink, minimum: 3, message: I18n.t("product.permalink.too_short_message")
 
   private
 
