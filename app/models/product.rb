060c31d1e2d6a5a3675c6e98cb73c99ba4ad0884
rishabh-vinsol <rishabh.kaushik@vinsol.com>
2023-04-03 11:44:14 +0530

    Changes in translation in product errors

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,21 +9,19 @@ class Product < ApplicationRecord
   has_many :orders, through: :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
-  validates_length_of :words_in_description, minimum: 5, maximum: 10,
-                                             too_short: I18n.t("product.description.too_short_message"),
-                                             too_long: I18n.t("product.desctiption.too_long_message")
+  validates_length_of :words_in_description, minimum: 5, maximum: 10
   validates :price, allow_blank: true, numericality: { greater_than: :discount_price }
   # validate :price_greater_than_discount_price
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, url: true
   validates :permalink, uniqueness: true, format: { with: PERMALINK_FORMAT }
-  validates_length_of :words_in_permalink, minimum: 3, message: I18n.t("product.permalink.too_short_message")
+  validates_length_of :words_in_permalink, minimum: 3
 
   private
 
   def price_greater_than_discount_price
     if price && discount_price
-      errors.add(:base, "Price must be greater than discount_price") unless price > discount_price
+      errors.add(:base, :price_less_than_discount) unless price > discount_price
     end
   end
 
