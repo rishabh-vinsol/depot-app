80e82adaba2b6548c80676fdd23fcaa3e4dd6b4c
rishabh-vinsol <rishabh.kaushik@vinsol.com>
2023-05-01 18:49:05 +0530

    Changed index action in Reports Controller

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -2,8 +2,6 @@ class Admin::ReportsController < AdminController
   def index
     start_date = params[:from].present? ? DateTime.parse(params[:from]) : 5.days.ago
     end_date = params[:to].present? ? DateTime.parse(params[:to]) : Time.current
-    start_date = DateTime.parse(params[:from])
-    end_date = DateTime.parse(params[:to])
     @orders = Order.where(created_at: start_date.beginning_of_day..end_date.end_of_day)
   end
 end
